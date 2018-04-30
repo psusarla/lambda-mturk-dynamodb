dca397e5fabdbc0f42fc78336e6a82bacbfc04b2
psusarla <phani.susarla@gmail.com>
2018-04-29 21:05:07 -0400

    Update TestMturkHitGenerator.java

--- src/test/java/com/premiumgeek/mturk/TestMturkHitGenerator.java
+++ src/test/java/com/premiumgeek/mturk/TestMturkHitGenerator.java
@@ -15,7 +15,7 @@ public class TestMturkHitGenerator {
 	@Test
 	public void retriveCvEntry() {
 		MturkHitGenerator mturk = new MturkHitGenerator();
-		CvUser cvUser = mturk.loadCvUserFromTable("fadarnell@gmail.com","12345a");
+		CvUser cvUser = mturk.loadCvUserFromTable("phani.susarla@gmail.com","12345a");
 		assertNotNull(cvUser);
 		System.out.println(cvUser.toString());
 	}
@@ -24,7 +24,7 @@ public class TestMturkHitGenerator {
 	public void testCreateHit() {
  		MturkHitGenerator mturk = new MturkHitGenerator();
 		MyHIT myHit = new MyHIT();
-		CvUser cvUser = mturk.loadCvUserFromTable("fadarnell@gmail.com","12345a");
+		CvUser cvUser = mturk.loadCvUserFromTable("phani.susarla@gmail.com","12345a");
 
         HIT hit = myHit.createHitUsingLayout("www.s3.com/myobject",cvUser);
         assertNotNull(hit);
@@ -35,7 +35,7 @@ public class TestMturkHitGenerator {
 	public void testCreateHit2() {
  		MturkHitGenerator mturk = new MturkHitGenerator();
 		MyHIT myHit = new MyHIT();
-		CvUser cvUser = mturk.loadCvUserFromTable("fadarnell@gmail.com","12345a");
+		CvUser cvUser = mturk.loadCvUserFromTable("phani.susarla@gmail.com","12345a");
 
         HIT hit = myHit.createHitUsingLayout("www.s3.com/myobject",cvUser);
         assertNotNull(hit);
@@ -62,7 +62,7 @@ public class TestMturkHitGenerator {
  		MturkHitGenerator mturk = new MturkHitGenerator();
  		
  		Map<String,String> envVars = new HashMap<String,String>();
- 		envVars.put("NOTIFICATION_ENDPOINT","https://sqs.us-east-1.amazonaws.com/321327483606/crowdvision-mturk-notifications");
+ 		envVars.put("NOTIFICATION_ENDPOINT","https://sqs.us-east-1.amazonaws.com/1234567890/crowdvision-mturk-notifications");
  		mturk.setEnvironmentVariables(envVars);
  		
         mturk.process("voicechecklist-temp", "023a12fa-56d0-4f1e-9e1a-968d114e3315.mp4_9f314e74-3785-4735-89f1-d2b761b4b07a");	
@@ -73,7 +73,7 @@ public class TestMturkHitGenerator {
  		MturkHitGenerator mturk = new MturkHitGenerator();
  		
  		Map<String,String> envVars = new HashMap<String,String>();
- 		envVars.put("NOTIFICATION_ENDPOINT","https://sqs.us-west-2.amazonaws.com/755956456552/mturk-queue-notification");
+ 		envVars.put("NOTIFICATION_ENDPOINT","https://sqs.us-west-2.amazonaws.com/1234567890/mturk-queue-notification");
  		mturk.setEnvironmentVariables(envVars);
  		
         mturk.process("voicechecklist-temp", "023a12fa-56d0-4f1e-9e1a-968d114e3315.mp4_aa4d2253-5a5a-438f-a976-8f7c5bf9175e");	
